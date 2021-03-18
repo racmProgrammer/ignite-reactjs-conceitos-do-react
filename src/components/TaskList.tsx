5ae9f08eabf5838190328add9134082320b770db
racmProgrammer <51178311+racmProgrammer@users.noreply.github.com>
2021-03-18 01:17:15 -0300

    Challange Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== ''){
+      let newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let alteredTasks = tasks.map((task: Task) =>{
+      
+      return task.id === id ? {...task, isComplete: !task.isComplete} : {...task};
+    });
+
+    setTasks(alteredTasks);
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const notRemovedTasks = tasks.filter((task) =>{
+      return task.id !== id;
+    });
+
+    setTasks(notRemovedTasks);
   }
 
   return (
